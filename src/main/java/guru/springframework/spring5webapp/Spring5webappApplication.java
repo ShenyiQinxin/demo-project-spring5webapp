15cacb1117f5a75177c63b5566860e3594b699ef
macolulu <shenyiqinxin@gmail.com>
2018-05-08 16:56:36 -0400

    added a comment

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -3,6 +3,9 @@ package guru.springframework.spring5webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * generated spring boot initiator
+ */
 @SpringBootApplication
 public class Spring5webappApplication {
 
