7eaf661c035671edd9f2e3448ce97d703c20b140
macolulu <shenyiqinxin@gmail.com>
2018-05-09 16:36:23 -0400

    added a comment

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -3,6 +3,21 @@ package guru.springframework.spring5webapp.model;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+/**
+ * http://www.baeldung.com/hibernate-many-to-many
+ * both the Employee class and Project classes refer to one another, which means that the association between them is bidirectional.
+
+ In order to map a many-to-many association, we use the @ManyToMany, @JoinTable and @JoinColumn annotations. Let’s have a closer look at them.
+
+ The @ManyToMany annotation is used in both classes to create the many-to-many relationship between the entities.
+
+ This association has two sides i.e. the owning side and the inverse side. In our example, the owning side is Employee so the join table is specified on the owning side by using the @JoinTable annotation in Employee class. The @JoinTable is used to define the join/link table. In this case, it is Employee_Project.
+
+ The @JoinColumn annotation is used to specify the join/linking column with the main table. Here, the join column is employee_id and project_id is the inverse join column since Project is on the inverse side of the relationship.
+
+ In the Project class, the mappedBy attribute is used in the @ManyToMany annotation to indicate that the employees collection is mapped by the projects collection of the owner side.
+ */
 @Entity
 public class Author {
 
@@ -13,6 +28,9 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
+    /*@ManyToMany
+    @JoinTable(name="author_book", joinColumns = @JoinColumn(name="author_id"),
+            inverseJoinColumns = @JoinColumn(name="book_id"))*/
     private Set<Book> books = new HashSet<>();
 
     public Author(){}
